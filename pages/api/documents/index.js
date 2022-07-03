3a48593a8fee68bb5faf2a4fd60b8aaf8f0e7e02
y.gouptar-ticket <yanissa.gt@orange.fr>
2022-07-03 13:55:13 -0400

    Ajout des routes /documents

---
+++ pages/api/documents/index.js
@@ -0,0 +1,33 @@
+import executeQuery from "../../../lib/db";
+
+//Liste des documents de l'étudiant
+export default async function handler(req, res) {
+    switch (req.method) {
+        case "GET":
+            try {
+                const result = await executeQuery({
+                    query: "select * from documents",
+                    values: []
+                })
+                res.status(200).json(result);
+            } catch (error) {
+                console.log(error);
+            }
+            break;
+
+        case "POST":
+            try {
+                const result = await executeQuery({
+                    query: "insert into documents(name, active) values (?,?)",
+                    values: [req.body.name, req.body.active]
+                })
+                res.status(200).json(result);
+            } catch (error) {
+                console.log(error);
+            }
+            break;
+
+        default:
+            return res.status(405).end('Method ${req.method} NotAllowed')
+    }
+}
