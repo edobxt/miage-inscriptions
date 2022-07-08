ee2eac55b6c2e2dda3ba43795887020fc762c5cb
Kancel Jonathan <kanjo.perso@gmail.com>
2022-07-08 08:40:51 -0400

    Mise en place d'une NavBar responsive et dynamique

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import styles from '../styles/Home.module.css'
-import {userService} from "services";
-import {Button} from "antd";
 
 export default function Home() {
   return (
     <>
-      <Button
-          type={"primary"}
-          onClick={() => userService.logout()}
-      >
-        Logout
-      </Button>
+        <h1>Page d&apos;accueil</h1>
     </>
   )
 }
